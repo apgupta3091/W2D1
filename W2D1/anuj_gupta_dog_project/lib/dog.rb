992b3bff137937d96aec4a321538c91c96d3d44d
Anuj Gupta <apgupta3091@gmail.com>
2021-10-11 11:52:17 -0400

    Completed the Dog Class

--- W2D1/anuj_gupta_dog_project/lib/dog.rb
+++ W2D1/anuj_gupta_dog_project/lib/dog.rb
@@ -1,3 +1,51 @@
 class Dog
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name 
+        @age = age
+        @breed = breed
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def name 
+        @name
+    end
+
+    def breed 
+        @breed
+    end
+    
+    def age 
+        @age 
+    end
+
+    def age=(age)
+        @age = age
+    end
+
+    def bark
+        if @age > 3
+            return @bark.upcase 
+        else 
+            return @bark.downcase 
+        end
+    end
+
+
+    def favorite_foods 
+        @favorite_foods
+    end
+
+
+    def  favorite_food?(str)
+        @favorite_foods.each do |food|
+            downcase_str = str.downcase
+            downcase_food = food.downcase
+            if downcase_str.include?(downcase_food)
+                return true
+            end
+        end
+        false
+    end
 
 end
